4702b003653f9424289b945c6a6dd116ea5ba259
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2024-08-01 05:02:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  cout<<"hey this is simple code ";
-  
+int main(){  
   int a;
-  int b;
   cin>>a;
-  cin>>b;
   cout<<"can you enter any number to chak if its negative or posative ";
   cin>>a;
 
   if(a<0){
     cout<<"its negative number ";
-  
+  }
+  else{
+    cout<<"its posetive number ";
+  }
+
   return 0;
 }
